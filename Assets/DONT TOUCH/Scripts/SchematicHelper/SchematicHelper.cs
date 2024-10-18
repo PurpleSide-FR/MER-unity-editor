b21f9e847b68219870d14fa76727e7915c6ddfbb
Morgan Coyaux <zmentionb@gmail.com>
2024-10-19 00:42:20 +0200

    it was just a test but i forgot to remove it from the code

--- Assets/DONT TOUCH/Scripts/SchematicHelper/SchematicHelper.cs
+++ Assets/DONT TOUCH/Scripts/SchematicHelper/SchematicHelper.cs
@@ -12,7 +12,6 @@ public class SchematicHelper : EditorWindow
     [MenuItem("MapEditorReborn Tools/Selection/Set Static")]
     private static void SetStaticSelected()
     {
-        EditorApplication.Beep();
         foreach (var go in Selection.gameObjects)
         {
             go.isStatic = true;
